256c6664fccbf0471607b2af2875cfba5404a565
Thivakar-T <thivakardravid1508@gmail.com>
2022-11-08 14:47:15 +0530

    commit

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,51 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContractComponent } from './contract/contract.component';
-import { VendorListComponent } from './vendor-list/vendor-list.component';
-import { SiteEngineerComponent } from './site-engineer/site-engineer.component';
-import { ContractorworkersComponent } from './contractorworkers/contractorworkers.component';
-import { OwnerComponent } from './owner/owner.component';
-import {ManagerListComponent} from './manager-list/manager-list.component';
-import {CreateManagerComponent} from './create-manager/create-manager.component';
-import { VendorDetailsComponent } from './vendor-details/vendor-details.component';
-import { SiteCreationComponent } from './sitecreation/sitecreation.component';
-import { SiteEngineerlistComponent } from './site-engineerlist/site-engineerlist.component';
-import { ContractlistComponent } from './contractlist/contractlist.component';
-import { ContractorworkerlistComponent } from'./contractorworkerlist/contractorworkerlist.component';
-import { OwnerlistComponent } from "../pages/ownerlist/ownerlist.component";
-import { MaterialListComponent } from './material-list/material-list.component';
-import { SiteCreationListComponent } from './site-creation-list/site-creation-list.component';
-import { VendorWisePurchaseComponent } from './vendor-wise-purchase/vendor-wise-purchase.component';
-import { VendorListWisePurchaseComponent } from './vendor-list-wise-purchase/vendor-list-wise-purchase.component';
 
 
 const routes: Routes = [
-  {path:"contract", component:ContractComponent},
-  {path:"contract/edit/:id", component:ContractComponent},
-  {path:"vendor", component:VendorListComponent},
-  {path:"siteEngineer", component:SiteEngineerComponent},
-  {path:"siteEngineerlist",component: SiteEngineerlistComponent},
-  {path:"contractorWorkers", component:ContractorworkersComponent},
-  {path:"vendorList", component:VendorListComponent},
-  {path:"sidenav/siteEngineer", component:SiteEngineerComponent},
-  {path:"managerList", component:ManagerListComponent},
-  {path:"createManager", component:CreateManagerComponent},
-  {path:"createManager/edit/:id",component:CreateManagerComponent},
-  {path:"vendorDetails", component:VendorDetailsComponent},
-  {path:"OwnerComponent", component:OwnerComponent},
-  {path:"ownerlist",component:OwnerlistComponent},
-  {path:"siteCreation",component:SiteCreationComponent},
-  {path:"ContractorworkerlistComponent",component:ContractorworkerlistComponent},
-  {path:"contractlist",component:ContractlistComponent},
-  {path:"vendorDetails/edit/:id",component:VendorDetailsComponent},
-  {path:"MaterialType", component:MaterialListComponent},
-  {path:"MaterialType/edit/:id",component:MaterialListComponent},
-  {path:"siteEngineer/edit/:id",component:SiteEngineerComponent},
-  {path:"SiteCreationList", component:SiteCreationListComponent},
-  {path:"siteCreation/edit/:id", component:SiteCreationComponent},
-  {path:"vendorWisePurchase", component:VendorWisePurchaseComponent},
-  {path:"vendorPurchaseList",component:VendorListWisePurchaseComponent},
-  {path:"vendorWisePurchase/edit/:id",component:VendorWisePurchaseComponent},
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule }   from '@angular/forms';
 import { PagesRoutingModule } from './pages-routing.module';
 import {NgbAlertModule,NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { VendorListComponent } from './vendor-list/vendor-list.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -12,46 +11,12 @@ import {HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
-import { CreateManagerComponent } from './create-manager/create-manager.component';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { OwnerComponent } from './owner/owner.component';
-import { ContractorworkersComponent } from './contractorworkers/contractorworkers.component';
-import { ManagerListComponent } from './manager-list/manager-list.component';
 import { TokenInterceptorService } from './../services/token-interceptor.service';
-import { SiteEngineerComponent } from './site-engineer/site-engineer.component';
-import { VendorDetailsComponent } from './vendor-details/vendor-details.component';
-import { SiteCreationComponent } from './sitecreation/sitecreation.component';
-import { SiteEngineerlistComponent } from './site-engineerlist/site-engineerlist.component';
-import { ContractComponent } from './contract/contract.component';
-import { ContractlistComponent } from './contractlist/contractlist.component';
-import { ContractorworkerlistComponent } from './contractorworkerlist/contractorworkerlist.component';
-import { OwnerlistComponent } from './ownerlist/ownerlist.component';
-import { MaterialListComponent } from './material-list/material-list.component';
-import { SiteCreationListComponent } from './site-creation-list/site-creation-list.component';
 import { NgxSpinnerModule } from "ngx-spinner";
-import { VendorWisePurchaseComponent } from './vendor-wise-purchase/vendor-wise-purchase.component';
-import { VendorListWisePurchaseComponent } from './vendor-list-wise-purchase/vendor-list-wise-purchase.component';
 
 @NgModule({
   declarations: [
-    VendorListComponent,
-    CreateManagerComponent,
-    ManagerListComponent,
-    VendorListComponent,
-    SiteEngineerComponent,
-    OwnerComponent,
-    ContractorworkersComponent,
-    VendorDetailsComponent,
-    SiteCreationComponent,
-    SiteEngineerlistComponent,
-    ContractComponent,
-    ContractlistComponent,
-    ContractorworkerlistComponent,
-    OwnerlistComponent,
-    MaterialListComponent,
-    SiteCreationListComponent,
-    VendorWisePurchaseComponent,
-    VendorListWisePurchaseComponent,
 
   ],
   imports: [
